56c04345a1d375a439e69d96eb9a11c625b8d4d8
mistakemp4 <sorenmyers@outlook.com>
2025-10-25 12:04:21 -0700

    chore: init

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+      server: {
+          watch: {
+              usePolling: true,
+          }
+      }
+  },
+
+  integrations: [alpinejs()]
+});
