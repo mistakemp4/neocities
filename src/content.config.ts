950b09e5d39d440f1f0fca1b5098ccd38c9942ff
mistakemp4 <sorenmyers@outlook.com>
2025-10-26 09:53:34 -0700

    refactor!: akechi quotes -> content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,8 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+import { file, glob } from 'astro/loaders';
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+    loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
@@ -10,6 +10,14 @@ const blog = defineCollection({
     }),
 });
 
+const akechi = defineCollection({
+    loader: file('./src/data/akechi.txt', {
+        parser: text => [...text.split('\n').entries()]
+            .flatMap(([line, quote]) => ({id: line, quote})),
+    }),
+});
+
 export const collections = {
+    akechi,
     blog,
 };
