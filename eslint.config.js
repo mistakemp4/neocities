31c1a51b2a7ade45fffd80b30f1b4bf108f2819f
mocha8686 <june.mocha8686@gmail.com>
2025-10-26 13:40:21 -0700

    style: update linter rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
 eslint.configs.recommended,
@@ -9,8 +10,15 @@ eslint.configs.recommended,
 eslintConfigPrettier,
 ...eslintPluginAstro.configs.recommended,
   {
-    rules: {},
+	  plugins: {
+		  "simple-import-sort": simpleImportSort,
+	  },
+    rules: {
+		"simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+	},
     ignores: [
+		"*.md",
       "package-lock.json",
       "dist/",
       ".astro/",
