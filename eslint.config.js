139ae985cbf3c03d57fc5da649bd979fc2df5924
mocha8686 <june.mocha8686@gmail.com>
2025-10-26 13:39:10 -0700

    fix: linters

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  ...eslintPluginAstro.configs.recommended,
+eslint.configs.recommended,
+...tseslint.configs.recommended,
+eslintConfigPrettier,
+...eslintPluginAstro.configs.recommended,
   {
     rules: {},
     ignores: [
