a7abb1666cfdad4c580bf2d822fb2216e9a1de97
mistakemp4 <sorenmyers@outlook.com>
2026-05-10 15:04:30 -0700

    feat: shrine redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,20 +21,43 @@ export default defineConfig({
 			},
 		},
 	},
-  experimental: {
-    fonts: [{
-		name: 'Persona 4',
-      provider: 'local',
-	  cssVariable: '--font-persona4',
-	  variants: [
-		{
-			src: [ './src/assets/fonts/FOT-SkipStd-B.otf' ],
-		},
-	  ],
-	  fallbacks: [ 'Courier New', 'monospace' ],
-    }],
-  },
+	experimental: {
+		fonts: [
+			{
+				name: 'Persona 4',
+				provider: 'local',
+				cssVariable: '--font-persona4',
+				variants: [
+					{
+						src: ['./src/assets/fonts/FOT-SkipStd-B.otf'],
+					},
+				],
+				fallbacks: ['Courier New', 'monospace'],
+			},
+			{
+				name: 'P5 Hatty',
+				provider: 'local',
+				cssVariable: '--font-p5-body',
+				variants: [
+					{
+						src: ['./src/assets/fonts/p5hatty-1.ttf'],
+					},
+				],
+				fallbacks: ['Georgia', 'serif'],
+			},
+			{
+				name: 'P5 Menu',
+				provider: 'local',
+				cssVariable: '--font-p5-header',
+				variants: [
+					{
+						src: ['./src/assets/fonts/Persona5MenuFontPrototype-Regular.ttf'],
+					},
+				],
+				fallbacks: ['Impact', 'sans-serif'],
+			},
+		],
+	},
 
 	integrations: [alpinejs()],
 });
-
